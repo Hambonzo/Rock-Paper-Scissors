81796057382d3a0685be8b3af5863c1df43ecc28
Hambonzo <ledham@outlook.com>
2025-09-17 14:51:09 +0100

    reworked logic for buttons and used DOM

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -10,61 +10,82 @@ function getComputerChoice() {
     } return "scissors";
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("What hand do you play?", "");
 
-    if (humanChoice === "rock") {
-        return "rock";
-    } else if (humanChoice === "paper") {
-        return "paper";
-    } else if (humanChoice === "scissors") {
-        return "scissors";
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+rock.addEventListener("click", () => {
+    playRound("rock", getComputerChoice());
+});
+
+paper.addEventListener("click", () => {
+    playRound("paper", getComputerChoice());
+});
+
+scissors.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice());
+});
+
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+function playRound(humanChoice, computerChoice) {
+    const results = document.querySelector("#results");
+    humanChoice = humanChoice.toLowerCase();
+    let message = "";
+
+    if (humanScore === 5 || computerScore === 5) return;
+
+    if (humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        message = "You win! " + humanChoice + " beats " + computerChoice;
+
+
+    } else if (computerChoice === "rock" && humanChoice === "scissors" ||
+        computerChoice === "paper" && humanChoice === "rock" ||
+        computerChoice === "scissors" && humanChoice === "paper") {
+        computerScore++;
+        message = "You lose! " + computerChoice + " beats " + humanChoice;
+    } else {
+        message = "Draw! Both drew same hand";
     }
+
+
+    let finalMessage = message;
+    if (humanScore === 5) finalMessage += "\n Humans win the game!";
+    else if (computerScore === 5) finalMessage += "\n Machines win the game!";
+
+    results.textContent = `${finalMessage}\nScore: Human ${humanScore}, Computer: ${computerScore}`;
 }
 
+/*
+ function playGame() {
 
+for (let i = 1; i <= 5; i++) {
+    console.log("Round: " + i + " fight!");
+    let humanChoice = getHumanChoice();
+    console.log("Humans chose: " + humanChoice);
+    let computerChoice = getComputerChoice();
+    console.log("Machines chose: " + computerChoice);
+    playRound(humanChoice, computerChoice);
+    console.log("Humans: " + humanScore + ", Machines: " + computerScore);
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    
-    function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.toLowerCase();
-
-        let winner = "You win " + humanChoice + " beats " + computerChoice + "!";
-        let loser = "You lose " + computerChoice + " beats " + humanChoice + " :(";
-
-        if (humanChoice === "rock" && computerChoice === "scissors" ||
-            humanChoice === "paper" && computerChoice === "rock" ||
-            humanChoice === "scissors" && computerChoice === "paper") {
-            return ++humanScore;
-
-
-        } else if (computerChoice === "rock" && humanChoice === "scissors" ||
-            computerChoice === "paper" && humanChoice === "rock" ||
-            computerChoice === "scissors" && humanChoice === "paper") {
-            return ++computerScore;
-
-
-        } return "Both drew the same hand, score remains the same";
-    }
-
-    for (let i = 1; i <= 5; i++) {
-        console.log("Round: " + i + " fight!");
-        let humanChoice = getHumanChoice();
-        console.log("Humans chose: " + humanChoice);
-        let computerChoice = getComputerChoice();
-        console.log("Machines chose: " + computerChoice);
-        playRound(humanChoice, computerChoice);
-        console.log("Humans: " + humanScore + ", Machines: " + computerScore);
-
-    } if (humanScore > computerScore) {
-        return "Humanity wins!";
-    } else if (humanScore < computerScore) {
-        return "The machines win!";
-    } else 
-        return "We are at a standstill, Draw!";
+} if (humanScore > computerScore) {
+    return "Humanity wins!";
+} else if (humanScore < computerScore) {
+    return "The machines win!";
+} else 
+    return "We are at a standstill, Draw!";
+} */
 
 
 
-}
+
+
+
